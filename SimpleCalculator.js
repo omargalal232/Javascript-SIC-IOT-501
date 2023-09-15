791bd059a52b3c101991ce723b9cdceb314ed4d0
omargalal232 <omargalal232@gmail.com>
2023-09-15 17:40:15 +0300

    Added calculator, a program (functior) that performs basic arithmetic operations (add, subtract, multiply, divide).

---
+++ SimpleCalculator.js
@@ -0,0 +1,18 @@
+function calculator(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "*":
+            return num1 * num2;
+        case "/":
+            if (num2 !== 0) {
+                return num1 / num2;
+            } else {
+                return "Cannot divide by zero";
+            }
+        default:
+            return "Invalid operator";
+    }
+}
