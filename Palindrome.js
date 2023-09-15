2565edfd5d3f8c2146362d4cd230075169449b1f
omargalal232 <omargalal232@gmail.com>
2023-09-15 17:32:16 +0300

    Added isPalindrome, a function that checks whether a given word or phrase is a palindrome.

---
+++ Palindrome.js
@@ -0,0 +1,5 @@
+function isPalindrome(str) {
+    const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedStr = cleanedStr.split('').reverse().join('');
+    return cleanedStr === reversedStr;
+}  
